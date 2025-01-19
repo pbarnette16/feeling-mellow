70f1103a3f358a0111bbc499eed770a6672bfe15
Paul Barnette <paul.barnette@gmail.com>
2025-01-19 16:51:14 +1100

    Storybook setup, plugins, setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,13 @@ import path from "path";
 import { DefinePlugin } from "webpack";
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+
 const config = {
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -20,15 +22,21 @@ const config = {
         projectRoot: "../",
       },
     },
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-webpack5-compiler-babel",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   webpackFinal: (config) => {
+    config.performance.maxAssetSize = 512000;
+    config.performance.maxEntrypointSize = 512000;
     config.plugins.push(
       // Used for surfacing information related to constants
       new DefinePlugin({
@@ -40,6 +48,10 @@ const config = {
 
     return config;
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 
 function sanitizeConfig(config) {
--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { colors } from './constants/colors'
 
-export default function App() {
+function App() {
+
+  const [fontsLoaded, fontError] = Font.useFonts({
+    "DM-Sands-Italic": require("./assets/fonts/DMSans-Italic-VariableFont_opsz,wght.ttf"),
+    "DM-Sands": require("./assets/fonts/DMSans-VariableFont_opsz,wght.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Platform Default</Text>
+      <Text style={{ fontFamily: "DM-Sands-Italic" }}>DM Sands Italic</Text>
+      <Text style={{ fontFamily: "DM-Sands" }}>DM Sands</Text>
     </View>
   );
+}
+
+let AppEntryPoint = App;
+
+if (process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true") {
+  AppEntryPoint = require("./.ondevice").default;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.Grey['lightest-grey'].value,
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "DM-Sands"
   },
 });
+
+export default AppEntryPoint;
